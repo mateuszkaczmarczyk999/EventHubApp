4710183fad451937ef87965c4eb882c5f272985c
math.k999 <math.k999@gmail.com>
2017-11-05 20:18:09 +0100

    Create Add Event form.

---
+++ EventHubApp/Controllers/EventController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventHubApp.Models;
+using EventHubApp.ViewModels;
+
+namespace EventHubApp.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EventController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel()
+            {
+                Genres = _db.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventHubApp/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventHubApp.Models;
+
+namespace EventHubApp.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
