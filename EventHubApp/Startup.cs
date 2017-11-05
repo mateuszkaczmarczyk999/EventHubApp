7aa4cc84bf9d9bbe4231d331cd4ac35313859978
math.k999 <math.k999@gmail.com>
2017-11-05 18:22:50 +0100

    Add project files.

---
+++ EventHubApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventHubApp.Startup))]
+namespace EventHubApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
